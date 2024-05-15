7ddf6dad9dbb19b00b14cee03144609e9ca7e2ae
muminovbob <bobomurodm2004@gmail.com>
2024-05-16 00:18:37 +0500

    feat: implemented paging logic

--- src/api/index.js
+++ src/api/index.js
@@ -3,9 +3,10 @@ import axios from "axios"
 
 const url = "https://rickandmortyapi.com/api"
 
-export async function getCharacters(){
+export async function getCharacters(page){
     try{
-        let res = await axios.get(`${url}/character`)
+        console.log(page)
+        let res = await axios.get(`${url}/character/?page=${page}`)
         console.log(res)
         if(res){
             return res.data
--- src/components/TheCardField.vue
+++ src/components/TheCardField.vue
@@ -1,21 +1,79 @@
 <script setup>
-import {ref, onMounted} from 'vue'
+import {ref, onMounted, computed} from 'vue'
 import { getCharacters } from '@/api';
+
+import { useRoute } from 'vue-router';
+import { useRouter } from 'vue-router';
 
 import MortyCard from './MortyCard.vue';
 
 const characters = ref(null)
+const info = ref(null)
+const route = useRoute()
+const router = useRouter()
+const page = ref(1)
 
-onMounted(async ()=>{
-    let data = await getCharacters()
+const handleNext = async () => {
+    page.value += 1
+    await fetchAndWrite()
+}
+
+const handlePrev = async () => {
+    page.value -= 1
+    await fetchAndWrite()
+}
+
+const fetchAndWrite = async () =>{
+    let data = await getCharacters(page.value)
     if(data){
         characters.value = data.results
+        info.value = data.info
     }
+} 
+
+const canPrev = computed(()=>{
+    if (info.value){
+        return info.value.prev == null
+    }
+    return true
+})
+
+const canNext = computed(()=>{
+    if (info.value){
+        return info.value.next == null
+    }
+    return true
+})
+
+const handlePageClick = async (e) =>{
+    page.value = e.srcElement._value
+    await fetchAndWrite()
+}
+
+
+onMounted(async ()=>{
+    await fetchAndWrite()
 })
 </script>
 
 <template>
-    <h2>sdads</h2>
+    <div class="paginator">
+        <input 
+            type="button" 
+            value="<<" 
+            @click="handlePrev" 
+            :disabled="canPrev"
+        >
+        <div class="nums">    
+            <input 
+                v-for="n in 5"
+                type="button" 
+                :value="page+(n-1)"
+                @click="handlePageClick"
+            >
+        </div>
+        <input type="button" value=">>" @click="handleNext" :disabled="canNext">
+    </div>
     <div class="cards">
         <MortyCard 
             v-for="card in characters" 
@@ -37,5 +95,15 @@ onMounted(async ()=>{
     flex-wrap: wrap;
     justify-content: space-around;
     gap:1em;
+}
+
+.paginator{
+    display: flex;
+    margin-bottom: 20px;
+}
+.nums{
+    display: flex;
+    margin-left: 3px;
+    margin-right: 3px;
 }
 </style>
